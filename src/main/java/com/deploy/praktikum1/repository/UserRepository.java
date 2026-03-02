c34d32ba5923ab3ea27c7875704d15689bf99cab
wpiskaa <hafizzkurniawan19@gmail.com>
2026-03-02 11:53:12 +0700

    Implementasi JpaRepository untuk operasi CRUD pada tabel User

---
+++ src/main/java/com/deploy/praktikum1/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.deploy.praktikum1.repository;
+
+import com.deploy.praktikum1.model.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, String> {
+}
