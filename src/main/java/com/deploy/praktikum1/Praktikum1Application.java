4c64246d1f7684bfa5a79180794569fa62e0e79a
wpiskaa <hafizzkurniawan19@gmail.com>
2026-03-02 11:52:34 +0700

    Inisialisasi class utama untuk menjalankan aplikasi Spring Boot

---
+++ src/main/java/com/deploy/praktikum1/Praktikum1Application.java
@@ -0,0 +1,13 @@
+package com.deploy.praktikum1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Praktikum1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Praktikum1Application.class, args);
+	}
+
+}
